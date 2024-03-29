c02c68564117d3d8e63c19777241c62d721a5f09
Hardik Bhasin <hardikbhasin11@gmail.com>
2024-03-29 19:16:54 +0530

    changes

--- fourth.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Printf("%d %b %x\n", 747, 747, 747)
-	fmt.Printf("%d %b %x\n", 911, 911, 911)
-	fmt.Printf("%d %b %x\n", 90210, 90210, 90210)
-}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hardik-bhasin07/golang
+
+go 1.22.1
